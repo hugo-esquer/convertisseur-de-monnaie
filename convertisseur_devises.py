7c57a7a440f6780d51709ef2cfa6f4bf5807caa3
hugo-esquer <hugo.esquer@laplateforme.io>
2023-11-30 10:50:35 +0100

    gestion des erreurs

--- convertisseur_devises.py
+++ convertisseur_devises.py
@@ -5,24 +5,37 @@ liste_devises = ["EUR", "IDR", "BGN", "ILS", "GBP", "DKK", "CAD", "JPY", "HUF", 
 
 conversion = 0
 historique = 0
+fichier_historique = open("historique.txt", "r")
 
 run = True
 while run:
+    
+    consutler_historique = input("Voulez-vous consulter l'historique? O/N \n")
+    if consutler_historique.upper() == "O":
+        print(fichier_historique.read())
+        fichier_historique.close()
+
     devise = input("Entrer une devise: ")
-    montant = float(input("Entrer un montant en chiffres: "))
+    try:
+        montant = float(input("Entrer un montant en chiffres: "))
+    except:
+        montant = 0
     devise_conversion = input("Entrer une devise de conversion: ")
 
     if devise.upper() not in liste_devises:
         print("La devise de départ n'est pas bonne.")
     elif devise_conversion.upper() not in liste_devises:
         print("La devise de conversion n'est pas bonne.")
+    elif montant == 0:
+        print("Entrez un montant valide")
     else:
         conversion = cr.convert(devise.upper(), devise_conversion.upper(), montant)
         print(conversion)
 
-    historique = f"{montant} {devise.upper()} -> {conversion} {devise_conversion.upper()}"
-    with open("historique.txt", "a") as fichier:
-        fichier.write(historique + "\n")
-    stop = input("continuer ? oui, non: \n")
-    if stop == "non":
+        historique = f"{montant} {devise.upper()} -> {conversion} {devise_conversion.upper()}"
+        with open("historique.txt", "a") as fichier:
+            fichier.write(historique + "\n")
+            
+    stop = input("continuer ? O/N: \n")
+    if stop.upper() == "N":
         run = False
